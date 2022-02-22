df9b3a11d09cdb6d9001c20eeea42f5b983be377
Lasitha Senanayake <lasitha.senanayake@gmail.com>
2022-02-22 08:03:14 +0530

    add new

--- plugins/hosting/hosting.php
+++ plugins/hosting/hosting.php
@@ -20,8 +20,8 @@ class Hosting {
                         $cmd ="mysqldump -h ".$dbconfig->mysql_server." -u ".$dbconfig->mysql_username." -p".$dbconfig->mysql_password." ". $database ." > ".escapeshellarg($backup_location);
                     else
                         $cmd ="mysqldump -h ".$dbconfig->mysql_server." -u ".$dbconfig->mysql_username." ". $database ." > ".escapeshellarg($backup_location);
-                    return $cmd;
-                    return shell_exec($cmd);
+                    //return $cmd;
+                    exec($cmd);
                     if(file_exists($backup_location)){
                         $filename =TENANT_RESOURCE_LOCATION. "/apps/davvag-hosting-console/backups/".$database.".zip";
                         if(file_exists($filename)){unlink($filename);};
