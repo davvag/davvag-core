d58304f25d5949b3d25e869cc7a211141d4226fa
Lasitha Senanayake <lasitha.senanayake@gmail.com>
2022-04-14 06:53:56 +0530

    hosting all the bb one

--- plugins/hosting/hosting.php
+++ plugins/hosting/hosting.php
@@ -2,8 +2,9 @@
 
 class Hosting {
     public static function BackupSystem(){
-        $backup_location_app=TENANT_RESOURCE_LOCATION. "/apps/davvag-hosting-console/backups/".DATASTORE_DOMAIN."_APP.zip";
-        $backup_location_data=TENANT_RESOURCE_LOCATION. "/apps/davvag-hosting-console/backups/".DATASTORE_DOMAIN."_DATA.zip";
+        $backup_location = MEDIA_FOLDER . "/backup/".  DATASTORE_DOMAIN . "/";
+        $backup_location_app=$backup_location."app_backup_".DATASTORE_DOMAIN."_".time().".zip";
+        $backup_location_data=$backup_location."media_backup_".DATASTORE_DOMAIN."_".time().".zip";
         self::ZIP(TENANT_RESOURCE_LOCATION,$backup_location_app,true);
         self::ZIP(MEDIA_FOLDER . "/".  DATASTORE_DOMAIN."/",$backup_location_data,true);
     }
@@ -14,19 +15,24 @@ class Hosting {
                     $database=str_replace(".","_",DATASTORE_DOMAIN);
                     $dbconfig = json_decode(file_get_contents(DB_CONFIG_FILE));
                     $database=$dbconfig->init_db.str_replace(".","_",DATASTORE_DOMAIN);
-                    $backup_location=TENANT_RESOURCE_LOCATION. "/apps/davvag-hosting-console/backups/".$database.".sql";
+                    $backup_location = MEDIA_FOLDER . "/backup/".  DATASTORE_DOMAIN . "/";
+                    if (!file_exists($backup_location))
+                        mkdir($backup_location, 0777, true);
+
+                    $backup_location_data_path = MEDIA_FOLDER . "/backup/".  DATASTORE_DOMAIN . "/".$database.".sql";
                     
                     if($dbconfig->mysql_password!="")
-                        $cmd ="mysqldump -h ".$dbconfig->mysql_server." -u ".$dbconfig->mysql_username." -p".$dbconfig->mysql_password." ". $database ." > ".escapeshellarg($backup_location);
+                        $cmd ="mysqldump -h ".$dbconfig->mysql_server." -u ".$dbconfig->mysql_username." -p".$dbconfig->mysql_password." ". $database ." > ".escapeshellarg($backup_location_data_path);
                     else
-                        $cmd ="mysqldump -h ".$dbconfig->mysql_server." -u ".$dbconfig->mysql_username." ". $database ." > ".escapeshellarg($backup_location);
+                        $cmd ="mysqldump -h ".$dbconfig->mysql_server." -u ".$dbconfig->mysql_username." ". $database ." > ".escapeshellarg($backup_location_data_path);
                     //return $cmd;
                     exec($cmd);
                     if(file_exists($backup_location)){
-                        $filename =TENANT_RESOURCE_LOCATION. "/apps/davvag-hosting-console/backups/".$database.".zip";
+                        $backup_location = MEDIA_FOLDER . "/backup/".  DATASTORE_DOMAIN . "/";
+                        $filename =$backup_location."db_backup_".$database."_".time().".zip";
                         if(file_exists($filename)){unlink($filename);};
                        
-                        self::ZIP($backup_location,$filename,false);
+                        self::ZIP($backup_location_data_path,$filename,false);
                         return true;
                     }else{
                         return false;
