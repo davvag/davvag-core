d06098939339a461812826289f904f52a7b49e9b
Lasitha Senanayake <lasitha.senanayake@gmail.com>
2023-07-05 07:07:12 +0530

    add all

--- plugins/sossdata/DataStore.php
+++ plugins/sossdata/DataStore.php
@@ -5,7 +5,7 @@ interface iDataStore {
     public function Update ($className, $saveObj, $tenantId = null);
     public function Delete ($className, $saveObj, $tenantId = null);
     public function Query($className, $query, $lastVersionId = null,$sorting="asc",$pageSize=20,$fromPage=0, $tenantId = null,$viewObject=true);
-    public function SetViewObject($objectID=0,$tenantId=0);
+    public function SetViewObject($objectID=0,$tenantId=null);
     public function Close($tenantId);
 }
 ?>
